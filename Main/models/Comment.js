ad81c38f3255733a4a258af61e3dbfc5bb5b182e
Isaac Luton <isaacdluton@gmail.com>
2022-06-22 17:49:07 -0700

    addition of comment model for db

---
+++ Main/models/Comment.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    comment_user: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+    },
+      comment_Info: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
--- Main/public/js/post.js
+++ Main/public/js/post.js
@@ -1,2 +1,69 @@
-const commentBtn = document.querySelector("commentBtn");
+const commentBtn = document.querySelector(".commentBtn");
+const modal = document.getElementById("comment-modal");
+const closeBtn = document.querySelector(".closeBtn");
+const form = document.getElementById("modalForm")
 
+commentBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+})
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none"
+})
+
+window.addEventListener("click", (e) => {
+    if(e.target === modal) {
+        modal.style.display = "none"; 
+    }
+})
+
+// delete
+
+
+// const delButtonHandler = async (event) => {
+//     if (event.target.hasAttribute('data-id')) {
+//       const id = event.target.getAttribute('data-id');
+  
+//       const response = await fetch(`/api/kanban/${id}`, {
+//         method: 'DELETE',
+//       });
+  
+//       if (response.ok) {
+//         document.location.replace('/api/kanban');
+//       } else {
+//         alert('Failed to delete project');
+//       }
+//     }
+//   };
+
+//   document.querySelectorAll(".dltCardBtn").forEach(e => e.addEventListener("click", delButtonHandler));
+
+// // create
+
+// const newTaskHandler = async (event) => {
+//   event.preventDefault();
+  
+//     const name = document.querySelector('#name').value.trim();
+//     const task = document.querySelector('#task').value.trim();
+//     const description = document.querySelector('#description').value.trim();
+  
+//     if (name && task && description) {
+
+//       const response = await fetch(`/api/kanban`, {
+//         method: 'POST',
+//         body: JSON.stringify({ task, description, name }),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       });
+  
+//       if (response.ok) {
+//         document.location.replace('/api/kanban');
+//       } else {
+//         alert('Failed to create project');
+//       }
+//     }
+//   };
+
+//   const modalSave = document.querySelector(".modalInputBtn")
+//   modalSave.addEventListener("click", newTaskHandler);
